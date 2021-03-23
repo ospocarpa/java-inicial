6284cc2846fb32776617d3db50e3700c4a17ccac
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-23 12:09:11 -0300

    mas tipos primitivos

---
+++ Fundamentos/tipoBoolean/src/tipoBoolean.java
@@ -0,0 +1,24 @@
+
+public class tipoBoolean {
+    public static void main(String[] args) {
+        boolean varBoolean = false;
+        System.out.println("varBoolean = " + varBoolean);   
+        if(varBoolean) {
+            System.out.println("la bandera es verdadera");
+           
+        }
+        else {
+            System.out.println("la bandera es falsa");
+        }
+        
+        var edad=10;
+        if(edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } 
+        else {
+            System.out.println("Eres menor de edad");
+        }
+        
+    }
+    
+}
