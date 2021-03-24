4ebae2f3c991f7491ddaba2d5245c319a3e24fa6
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-24 00:13:39 -0300

    operadores aritemicos-asignacion

---
+++ Fundamentos/operadoresAritmeticos/src/operadoresAritmeticos.java
@@ -0,0 +1,27 @@
+
+public class operadoresAritmeticos {
+    public static void main(String[] args) {
+        int a=3,b=2;
+        var resultado = a+b;
+        System.out.println("resultado suma = " + resultado);
+        resultado = a-b;
+        System.out.println("resultado resta = " + resultado);
+        resultado = a*b;
+        System.out.println("resultado multiplicacion = " + resultado );
+        
+        var resultado2=3D/b;
+        System.out.println("resultado division = " + resultado2);
+        resultado = a%b;
+        System.out.println("resultado modulo = " + resultado);
+        // es Par a ?
+        if (a%2 == 0){
+            System.out.println("Es par");
+        } 
+        else {
+            System.out.println("Es impar");
+            
+        }
+        
+    }
+    
+}
