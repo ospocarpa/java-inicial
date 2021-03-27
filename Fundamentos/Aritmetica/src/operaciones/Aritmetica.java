701b4164a3314dd35e99d9f9d11af0568615cf2b
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-27 11:53:58 -0300

    se crea metodo con argumentos

---
+++ Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,22 @@
+
+package operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    //Metodo
+    public void sumar() {
+        int resultado= a +b;
+        System.out.println("resultado = " + resultado);
+    }
+    public int sumarConRetorno(){
+        return a+b;
+    }
+    public int sumarConArgumentos(int arg1,int arg2) {
+        a=arg1;
+        b=arg2;
+        return sumarConRetorno();
+    }
+}
