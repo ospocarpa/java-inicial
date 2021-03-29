bd700ab10e25c5d8a9dcc527bb2ab237e36df5dc
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-29 12:10:59 -0300

    probando alcande de variables

--- Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
+++ Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -3,6 +3,12 @@ package operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        // variables locales
+        var a=10;
+        var b=2;
+        miMetodo();
+        
+        
         Aritmetica aritmetica1= new Aritmetica();
         System.out.println("aritmetica1 a:"+aritmetica1.a);
           System.out.println("aritmetica1 b:"+aritmetica1.b);
@@ -12,5 +18,13 @@ public class PruebaAritmetica {
           System.out.println("aritmetica2 b:"+aritmetica2.b);
         
  
+    }
+    
+    // los metodos estaticos solo pueden ser accedidos por 
+    // otros metodos estaticos
+    public static void miMetodo() {
+        System.out.println("Ejecutando otro metodo");
+        //la variable esta fuera del alcance donde fue definida
+        // a=10;
     }
 }
