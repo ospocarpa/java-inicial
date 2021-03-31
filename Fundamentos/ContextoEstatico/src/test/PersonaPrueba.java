9ce943d1ed3192165bb941bac6ebcf3dd2ab2dd6
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-30 22:17:17 -0300

    mas de contexto estatico

--- Fundamentos/ContextoEstatico/src/test/PersonaPrueba.java
+++ Fundamentos/ContextoEstatico/src/test/PersonaPrueba.java
@@ -7,9 +7,13 @@ import domain.Persona;
 public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan");
-        System.out.println("persona1 = " + persona1);
-        
         Persona persona2 = new Persona("Karla");
-        System.out.println("persona2 = " + persona2);
+        imprimir(persona1);
+        imprimir(persona2);
+        
+    }
+    public static void imprimir(Persona persona) 
+    {
+        System.out.println("persona = " + persona);
     }
 }
