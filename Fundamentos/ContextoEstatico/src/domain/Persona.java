204b8e5e97616d2e9f6edab922641e8b62bfe315
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-03-30 22:03:12 -0300

    probando contesto estatico

--- Fundamentos/ContextoEstatico/src/domain/Persona.java
+++ Fundamentos/ContextoEstatico/src/domain/Persona.java
@@ -1,33 +1,28 @@
-
 package domain;
-
 
 public class Persona {
 
-    public static int getContadorPersona() {
-        return contadorPersona;
-    }
-
-    public static void setContadorPersona(int aContadorPersona) {
-        contadorPersona = aContadorPersona;
-    }
     private int idPersona;
     private String nombre;
     private static int contadorPersona;
-    
+
     public Persona(String nombre) {
-        this.nombre=nombre;
+        this.nombre = nombre;
         //Incrementar el contador por cada objeto nuevo
         Persona.contadorPersona++;
         //Asignar el nuevo valor a la variable idPersona
         this.idPersona = Persona.contadorPersona;
     }
 
-    public String toString() {
-        return "Persona:[idPersona: " + this.nombre + " nombre: " 
-                +this.nombre ", contadorPersona: " +
-                this.contadorPersona "]" ;
+    public Persona() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override  //indica q estamos modificando este metodo definido en la clase object
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
+
 
     public int getIdPersona() {
         return idPersona;
@@ -44,4 +39,13 @@ public class Persona {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public static int getContadorPersona() {
+        return contadorPersona;
+    }
+
+    public static void setContadorPersona(int aContadorPersona) {
+        contadorPersona = aContadorPersona;
+    }
+
 }
---
+++ Fundamentos/ContextoEstatico/src/test/PersonaPrueba.java
@@ -0,0 +1,15 @@
+
+package test;
+
+import domain.Persona;
+
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan");
+        System.out.println("persona1 = " + persona1);
+        
+        Persona persona2 = new Persona("Karla");
+        System.out.println("persona2 = " + persona2);
+    }
+}
