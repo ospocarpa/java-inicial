37d8fe2f8fa0dcbc8311d88da1f91aca1eea3859
Oscar Hinojosa <you@example.com>
2021-05-12 06:41:48 +0100

    tarea2

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args) {
         
+
+//Debes imprimir la información en el siguiente formato:
+//
+//    Proporciona el titulo:
+//    Proporciona el autor:
+//    <titulo> fue escrito por <autor>
+
+        String titulo;String autor;
         Scanner consola = new Scanner(System.in);
-        System.out.println("Escribe tu nombre");
-        var nombre = consola.nextLine();
-        System.out.println("nombre = " + nombre);
-        System.out.println("Escribe un titulo");
-        var titulo = consola.nextLine();
-        System.out.println("resultado = " + titulo + " "+nombre);
-        
+        System.out.println("Proporcione el titulo");
+        titulo =consola.nextLine();
+         System.out.println("Proporcione el autor");
+        autor = consola.nextLine();
+        System.out.println(titulo + " fue escrito por " +autor);
+
             
     }
     
