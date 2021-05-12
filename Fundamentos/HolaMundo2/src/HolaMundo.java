e0bcf1203f8aabd0ee9be9f12eaa91743764b397
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-12 01:04:10 -0300

    concatenacion

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -2,22 +2,16 @@
 public class HolaMundo {
     public static void main(String[] args) {
         
-        int unEntero = 5;
-        System.out.println(unEntero);
-        unEntero = 10;
-        System.out.println(unEntero);
+        var usuario = "Oscar";
+        var titulo = "Estudiante ";
+        var union = titulo + usuario;
+        System.out.println("union = " + union);
         
-        String unaCadena = "Saludos";
-        System.out.println(unaCadena);
-        
-       unaCadena ="Adios";
-        System.out.println(unaCadena);
-        //
-        var unaCadena2 ="Nueva Cadena";  //  
-        System.out.println("unaCadena2 = " + unaCadena2);
-        
-        var unEntero2 = 12;
-        System.out.println("unEntero2 = " + unEntero2);
+        var i=3;var j=4;
+        System.out.println(i+j);
+        System.out.println(i+j+usuario);
+        System.out.println(usuario+i+j);
+        System.out.println(usuario + (i+j));
         
             
     }
