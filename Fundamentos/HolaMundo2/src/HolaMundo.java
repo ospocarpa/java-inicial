8f0879eb0f83564375ba1a9af04e2708f607fc15
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-18 18:55:50 -0300

    tipos flotantes

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -6,26 +6,15 @@ public class HolaMundo {
     public static void main(String[] args) {
         
 
-        byte numeroByte = (byte)130;
-        System.out.println("numeroByte = " + numeroByte);
-        System.out.println("Valor minimo byte:"+Byte.MIN_VALUE);
-        System.out.println("Valor maximo byte:"+Byte.MAX_VALUE);
+      float unFloat = 2.5F;
+        System.out.println("unFloat = " + unFloat);
+        System.out.println("Valor minimo float:"+Float.MIN_VALUE);
+        System.out.println("Valor maximo float:"+Float.MAX_VALUE);
         
-        short unShort =(short) 32768;
-        System.out.println("unShort = " + unShort);
-        System.out.println("Valor minimo short:"+Short.MIN_VALUE);
-        System.out.println("Valor maximo short:"+Short.MAX_VALUE);
-        
-        int unInt =  (int)2147483648L;
-        System.out.println("unInt = " + unInt);
-        
-        System.out.println("Valor minimo int:"+Integer.MIN_VALUE);
-        System.out.println("Valor maximo int:"+Integer.MAX_VALUE);
-        
-        long unLong = 9223372036854775807L;
-        System.out.println("unLong = " + unLong);
-        System.out.println("Valor minimo long:"+Long.MIN_VALUE);
-        System.out.println("Valor maximo long:"+Long.MAX_VALUE);
+        double unDouble = 10;
+        System.out.println("unDouble = " + unDouble);
+        System.out.println("Valor minimo double:"+Float.MIN_VALUE);
+        System.out.println("Valor maximo double:"+Float.MAX_VALUE);
         
         
         
