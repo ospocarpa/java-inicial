1f1709e1fb8be4331abd4aa0d4541bec7363ae49
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-18 21:11:01 -0300

    tipo boolean

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -6,35 +6,25 @@ public class HolaMundo {
     public static void main(String[] args) {
         
 
-        char miCaracter = 'A';
-        System.out.println("miCaracter = " + miCaracter);
+       boolean unBooleano = true;
+        System.out.println("unBooleano = " + unBooleano);
         
-        char varChar = '\u0040';
-        System.out.println("varChar = " + varChar);
+        if(unBooleano) {
+            System.out.println("La bandera es verdadera");
+        }
+        else {
+            System.out.println("La bandera es falsa");
+        }
         
-        char varDecimal = 64;
-        System.out.println("varDecimal = " + varDecimal);
-        
-        char varSimbolo = '@';
-        System.out.println("varSimbolo = " + varSimbolo);
-        
-        var varChar1 = '\u0040';
-        System.out.println("varChar1 = " + varChar1);
-        
-        var varDecimal2 = 64;
-        System.out.println("varDecimal2 = " + varDecimal2);
-        
-        var varSimbolo3 = '@';
-        System.out.println("varSimbolo3 = " + varSimbolo3);
-        
-        int variableEnteraSimbolo = '@';
-        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
-      
-       int letra  = 'A';
-        System.out.println("letra = " + letra);
-        
-         
+        int edadLupi=26; 
+        if(edadLupi>=18) {
+            System.out.println("Lupi es mayor de edad");
+        }
+        else {
+            System.out.println("Lupi es menor de edad");
+        }
+  
+    
+    
     }
-    
-    
 }
