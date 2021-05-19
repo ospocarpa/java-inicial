56cc44dbaebd14d03611720aaab57f3268177b5c
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-19 02:20:36 -0300

    operadores aritmeticos

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -1,25 +1,24 @@
 
-import java.util.Scanner;
+
 
 
 public class HolaMundo {
     public static void main(String[] args) {
     
-        Scanner consola = new Scanner(System.in);
-        System.out.println("Proporciona el nombre");
-        String nombre = consola.nextLine();
-        System.out.println("Proporciona el id");
-      
-        int id = Integer.parseInt(consola.nextLine());
-          System.out.println("Proporciona el precio");
-        double precio= Double.parseDouble(consola.nextLine());
-          System.out.println("Proporciona el envio gratuito");
-          boolean envioGratuito =Boolean.parseBoolean(consola.nextLine());
-        System.out.println("-----------------");
-        System.out.println(nombre+" #"+id);
-        System.out.println("Precio: $"+precio);
-        System.out.println("Envio Gratuito: "+envioGratuito);
-        
-        
+        int a =3,b=2;
+        var resultado = a +b;
+        System.out.println("resultado = " + resultado);
+        resultado =a-b;
+        System.out.println("resultado = " + resultado);
+        resultado = a*b;
+        System.out.println("resultado = " + resultado);
+        var resultado2=(double)a /b;
+        System.out.println("resultado2 = " + resultado2);
+        resultado = a%b;
+        System.out.println("resultado = " + resultado);
+        if(a %2 == 0)
+            System.out.println(a + " Es par");
+        else
+            System.out.println(a +" es impar");
     }
 }
