e8accf3160ba994819fec9c7244a157151b11ba9
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-19 02:32:10 -0300

    operadores de asignacion

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -5,20 +5,15 @@
 public class HolaMundo {
     public static void main(String[] args) {
     
-        int a =3,b=2;
-        var resultado = a +b;
-        System.out.println("resultado = " + resultado);
-        resultado =a-b;
-        System.out.println("resultado = " + resultado);
-        resultado = a*b;
-        System.out.println("resultado = " + resultado);
-        var resultado2=(double)a /b;
-        System.out.println("resultado2 = " + resultado2);
-        resultado = a%b;
-        System.out.println("resultado = " + resultado);
-        if(a %2 == 0)
-            System.out.println(a + " Es par");
-        else
-            System.out.println(a +" es impar");
+        int a=7;
+        a /=2;
+        System.out.println("a = " + a);
+        a/=2D;
+        System.out.println("a = " + a);
+        a%=2;
+        System.out.println("a = " + a);
+        a*=5;
+        System.out.println("a = " + a);
+        
     }
 }
