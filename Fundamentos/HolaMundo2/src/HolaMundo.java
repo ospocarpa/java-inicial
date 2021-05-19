8079532b5b8608af37b9efa64e4db70223a9cff4
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-19 02:04:15 -0300

    tienda de libro

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
+    
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Proporciona el nombre");
+        String nombre = consola.nextLine();
+        System.out.println("Proporciona el id");
+      
+        int id = Integer.parseInt(consola.nextLine());
+          System.out.println("Proporciona el precio");
+        double precio= Double.parseDouble(consola.nextLine());
+          System.out.println("Proporciona el envio gratuito");
+          boolean envioGratuito =Boolean.parseBoolean(consola.nextLine());
+        System.out.println("-----------------");
+        System.out.println(nombre+" #"+id);
+        System.out.println("Precio: $"+precio);
+        System.out.println("Envio Gratuito: "+envioGratuito);
         
-
-       var edad = Integer.parseInt("20");
-        System.out.println("edad = " + (edad+1));
-       var PI = Double.parseDouble("3.1416");
-        System.out.println("PI = " + PI);
-       // pedir un valor 
-       
-     Scanner consola = new Scanner(System.in);
-//        System.out.println("Proporcine su edad:");
-//     var valor = Integer.parseInt(consola.nextLine());
-//        System.out.println("valor = " + valor);   
-//        
         
-        var edadTexto = String.valueOf(25);
-        System.out.println("edadTexto = " + edadTexto);
-        
-        var caracter="hola".charAt(1);
-        System.out.println("Proporcione un caracter");
-        caracter = consola.nextLine().charAt(0);
-        System.out.println("caracter = " + caracter);
     }
 }
