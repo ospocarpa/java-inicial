4d3198d44e0e6038efedeac24887bd63ec708953
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-21 12:58:09 -0300

    rectangulo

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -1,13 +1,20 @@
 
+import java.util.Scanner;
+
+
 public class HolaMundo {
 
     public static void main(String[] args) {
 
-        var resultado = 5 > 4 ? "verdadero" : "falso";
-        System.out.println("resultado = " + resultado);
-
-        int numero = 4;
-        resultado = (numero % 2 == 0) ?"Es par":"Es impar";
-        System.out.println("resultado = " + resultado);
+      int alto,ancho;
+      Scanner consola = new Scanner(System.in);
+        System.out.println("Proporcione el alto");
+        alto = Integer.parseInt(consola.nextLine());
+       
+        System.out.println("Proporcione el ancho");
+         ancho = Integer.parseInt(consola.nextLine());
+        System.out.println("Area: "+alto*ancho);
+        System.out.println("Perimentro: "+(alto+ancho)*2);
+                 
     }
 }
