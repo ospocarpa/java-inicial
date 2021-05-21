1599aad4b1a9f03e01ea159dc55360b1d88d33fa
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-21 03:10:49 -0300

    operadores relacionales

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -5,15 +5,18 @@
 public class HolaMundo {
     public static void main(String[] args) {
     
-        int a=7;
-        a /=2;
-        System.out.println("a = " + a);
-        a/=2D;
-        System.out.println("a = " + a);
-        a%=2;
-        System.out.println("a = " + a);
-        a*=5;
-        System.out.println("a = " + a);
-        
+      // operadores relaciones
+       int a =4 ,b=3;
+       
+        var c = (a == b);
+        System.out.println("c = " + c);
+        var d = a!=b;
+        System.out.println("d = " + d);
+        String cadena ="Hola";
+        String cadena2="Hola";
+        var e = cadena == cadena2;
+        System.out.println("e = " + e);
+        var f = cadena.equals(cadena2);
+        System.out.println("f = " + f);
     }
 }
