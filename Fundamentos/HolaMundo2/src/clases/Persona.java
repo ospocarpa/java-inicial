ef1bca56f870d710164b0133a1d81ef91811c18d
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-21 19:32:49 -0300

    creando atributos y un metodo a Persona

--- Fundamentos/HolaMundo2/src/clases/Persona.java
+++ Fundamentos/HolaMundo2/src/clases/Persona.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clases;
 
-/**
- *
- * @author Usuario
- */
 public class Persona {
+    String nombre;
+    String apellido;
     
+    public void datosPersona() {
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellido = " + apellido);
+    }
 }
