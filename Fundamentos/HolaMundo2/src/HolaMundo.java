33fd38a994474a0dd7ef527fab18b045e46d2065
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-21 19:02:46 -0300

    usando break y continue en ciclos for

--- Fundamentos/HolaMundo2/src/HolaMundo.java
+++ Fundamentos/HolaMundo2/src/HolaMundo.java
@@ -5,32 +5,21 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String[] args) {
-
-      int numero;
-      var numeroTexto = "valor desconocido";
-      Scanner consola = new Scanner(System.in);
-        System.out.println("Ingrese un numero");
-      numero = Integer.parseInt(consola.nextLine());
-      
-      switch(numero) {
-          case 1:
-              numeroTexto="numero uno";
-         
-              break;
-           case 2:
-              numeroTexto="numero dos";
-              break;
-               case 3:
-              numeroTexto="numero tres";
-              break;
-               case 4:
-              numeroTexto="numero cuatro";
-              break;
-               default :
-                   numeroTexto = "numero no coincide";
-                   
-      }
-        System.out.println("numeroTexto = " + numeroTexto);
+        
+        System.out.println("Usando break");
+        for(int i=0;i<6;i++) {
+            if(i%2 == 0  ) {
+                System.out.println("i = " + i);
+                break;
+            }
+        }
+        System.out.println("Usando continue");
+        for(int i =0;i<6;i++) {
+            if(i%2 != 0) {
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
     
                  
     }
