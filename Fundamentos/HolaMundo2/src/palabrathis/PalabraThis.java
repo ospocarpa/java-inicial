71e0378f84ac5b33f88f39c6dcfcb900e471b1ad
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-22 19:08:56 -0300

    mas de this

--- Fundamentos/HolaMundo2/src/palabrathis/PalabraThis.java
+++ Fundamentos/HolaMundo2/src/palabrathis/PalabraThis.java
@@ -12,12 +12,30 @@ public class PalabraThis {
 // En un archivo solo puedo tener una clase publica
    //esta clase y constructor solo puedo usar dentro del mismo package que fue
  //definido 
+//
  class Persona {
-     String nombre;
+     String nombre; //estos atributos se conocen como default o package
      String apellido;
      Persona(String nombre,String apellido)  {
+         //super() :llamada al constructor de la clase padre(Object)
          this.nombre = nombre;
          this.apellido = apellido;
+         System.out.println("Objeto persona usando this:"+this);
+         new Imprimir().imprimir(this); //Imprmir imprimir= new Imprimir()  ---> equivalente
      }
     
+}
+class Imprimir {
+    // su constructor vacio llama al constructor de la clase object (con super() para reservar memoria
+    // todo lo hace implicitamente
+    /* 
+      public Imprimir() {
+        super();
+    }
+    */
+    
+    public void imprimir(Persona persona) {
+        System.out.println("persona desde imprimir: "+persona);
+        System.out.println("Impresion del objeto actual(this) "+ this);
+    }
 }
