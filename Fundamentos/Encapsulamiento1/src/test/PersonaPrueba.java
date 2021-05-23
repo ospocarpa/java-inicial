8b25c3a2a8e5dfd27141b3f47f98d33a2e25e49b
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2021-05-23 01:59:58 -0300

    otra forma de llamar a toString al existir el metodo usando el pritln y nombrando al objeto llama a dicho metodo

--- Fundamentos/Encapsulamiento1/src/test/PersonaPrueba.java
+++ Fundamentos/Encapsulamiento1/src/test/PersonaPrueba.java
@@ -5,19 +5,19 @@ import dominio.Persona;
 public class PersonaPrueba { //hola
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan",5000.00,false);
-        System.out.println("persona1:" + persona1.toString());
+        System.out.println("persona1:" + persona1);
         persona1.setNombre("Jose Maria");
         
-        System.out.println("persona1: "+persona1.toString());
+        System.out.println("persona1: "+persona1);
          
-        System.out.println("--------------------------");
+        
         Persona persona2= new Persona("Oscar Hinojosa",80000,false);
-        System.out.println("persona2 = " + persona2.toString());
+        System.out.println("persona2 = " + persona2);
         persona2.setNombre("NIcole martinez");
         persona2.setSueldo(50000);
         persona2.setEliminado(true);
-        System.out.println("----");
-        System.out.println("persona2 = " + persona2.toString());
+        
+        System.out.println("persona2 = " + persona2);
        
         
         
