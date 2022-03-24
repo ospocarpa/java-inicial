8fcc1f3f20f16a3576b09fa3cb1161e7a53f2ef5
Oscar Hinojosa <oscarhinojosacampos@est.frba.utn.edu.ar>
2022-03-24 19:35:22 -0300

    mejoro .gitnore se retoma practica de java de cero

---
+++ Fundamentos2/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,8 @@
+//teclas rapidas : psvm --> funcion principal sout-->println
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+ 
+}
