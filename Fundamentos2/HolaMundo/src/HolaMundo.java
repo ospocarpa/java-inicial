146c7229da991e647bff69bac601276273c86106
ospocarpa <oscarhinnojosa@gmail.com>
2022-06-15 02:40:47 -0300

    limpio repositorio

--- Fundamentos2/HolaMundo/src/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-//teclas rapidas : psvm --> funcion principal sout-->println
-
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
- 
-}
